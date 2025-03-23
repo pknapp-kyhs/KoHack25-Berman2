87a2944e6fe083f0e666fe1764cc5fe55a89d820
Akiva Cohen <Akivaecohen@gmail.com>
2025-03-23 15:49:41 -0400

    updates

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ArrayList<Question> questionsList = DataReader.getEveryQuestion();
+        Question[] questions = new Question[questionsList.size()];
+        questionsList.toArray(questions);
+        Quiz quiz = new Quiz(DataReader.getEveryQuestion().stream().toArray());
+        quiz.run();
     }
 }
