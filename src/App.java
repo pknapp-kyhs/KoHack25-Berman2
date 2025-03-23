4bd0b283622ae18e72fab1d163cca896237c7cc7
jxds99 <yehuda.dachman@mjbha.org>
2025-03-23 13:34:24 -0400

    debuging

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.print(DataReader.getRawResults());
-        System.out.print(DataReader.getQuestions(DataReader.getRawResults()));
+        System.out.print(DataReader.getQuestionText(DataReader.getRawResults()));
     }
 }
--- src/DataReader.java
+++ src/DataReader.java
@@ -1,32 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-public class DataReader{
-    private File dataFile = new File("Data.txt");
-    private Scanner scanner = new Scanner(dataFile);
+import java.util.ArrayList;
 
+public class DataReader{
+    private static File dataFile = new File("Data.txt");
+    
+    private static Scanner scanner;
+
+    static {
+        try {
+            scanner = new Scanner("Data.txt");
+        } catch (FileNotFoundException e) {
+            //do nothing
+        }
+    }
     //gets raw text results from data file
-    public ArrayList<String> getRawResults()
+    public static ArrayList<String> getRawResults()
     {
         ArrayList<String> rawResults = new ArrayList<String>();
-        while(scanner.hasNextLine)
+        while(scanner.hasNextLine())
         {
             rawResults.add(scanner.nextLine());
         }
         return rawResults;
     }
-    public ArrayList<Question> getQuestions(ArrayList<String> data)
+    public static ArrayList<String> getQuestionText(ArrayList<String> data)
     {
         ArrayList<String> questionList = new ArrayList<String>();
         for(int i = 0; i < data.size(); i++)
         {
-            questionList.add(data.get(i).substring(0,indexOf("?")));
+            questionList.add(data.get(i).substring(0,data.get(i).indexOf("?")));
         }
-        
+        return questionList;
     }
-    public String getJustAnswer(String data)
+    private String getJustAnswer(String data)
     {
-        return data.substring(indexOf("?")+1, data.length());
+        return data.substring(data.indexOf("?")+1, data.length());
     }
     
 }
