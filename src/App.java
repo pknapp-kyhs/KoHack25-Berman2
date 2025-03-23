24eb9d603c030edabf36047598712a1d2bdf220b
Akiva Cohen <Akivaecohen@gmail.com>
2025-03-23 15:50:36 -0400

    update test

--- src/App.java
+++ src/App.java
@@ -2,10 +2,8 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<Question> questionsList = DataReader.getEveryQuestion();
-        Question[] questions = new Question[questionsList.size()];
-        questionsList.toArray(questions);
-        Quiz quiz = new Quiz(DataReader.getEveryQuestion().stream().toArray());
+        Question[] questions = new Question[0];
+        Quiz quiz = new Quiz(DataReader.getEveryQuestion().toArray(questions));
         quiz.run();
     }
 }
