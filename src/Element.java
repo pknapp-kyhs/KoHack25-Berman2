dea6755e3ed28476228b78348ad038bfa1a2449f
jxds99 <yehuda.dachman@mjbha.org>
2025-03-23 11:27:42 -0400

    synnc element

---
+++ src/Element.java
@@ -0,0 +1,13 @@
+public class Element{
+    //this will contain the content for this element
+    private String content;
+    private 
+    public Element(String content){
+        this.content = content;
+    }
+    //This will present this element
+    public void presentElement(){
+        //use text to speech to say the content
+        //listen for an answer from the user
+    }
+}
