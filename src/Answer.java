7564f689b042d87a90c9d08d4f1c10c2a80e9cc0
Akiva Cohen <Akivaecohen@gmail.com>
2025-03-23 12:16:05 -0400

    project update

--- src/Answer.java
+++ src/Answer.java
@@ -1,3 +1,21 @@
+//a class to hold a potential answer to a question
 public class Answer {
+    int[] result;
+    String text;
     
+    public Answer(int[] result, String text) {
+        //checks parameter validity
+        if (result.length != Quiz.RESOLUTION) {
+            throw new IllegalArgumentException("Question modifier is wrong length");
+        }
+        this.text = text;
+        this.result = result;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public int[] getResult() {
+        return result;
+    }
 }
--- src/Question.java
+++ src/Question.java
@@ -1,18 +1,34 @@
 import java.security.InvalidParameterException;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Question extends JPanel {
     //maximum options per question
-    public static final int MAX_OPTIONS = 4;
+    public static final int MAX_OPTIONS = 5;
     String question;
     Answer[] answers;
 
     public Question(String question, Answer[] answers) {
+        super(new GridLayout(MAX_OPTIONS + 1,1));
         if (answers.length > MAX_OPTIONS) {
             throw new InvalidParameterException("Too many options in this question");
         }
+        //add the question at the top
+        add(new JTextArea(question));
+        JC
+        for (int i = 0; i < MAX_OPTIONS; i++) {
+            
+        }
         this.question = question;
         this.answers = answers;
-    }    
+    }
+    
+    public int[] ask() {
+
+    }
+
+    private void build() {
+
+    }
 }
