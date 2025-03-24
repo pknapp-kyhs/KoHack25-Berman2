d31058fb8eb83f543d03f86348a97bda5634951b
Akiva Cohen <Akivaecohen@gmail.com>
2025-03-24 16:09:01 -0400

    comments and remove unnecesarry lines

--- src/Question.java
+++ src/Question.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.io.*;
 import java.awt.*;
-import javax.imageio.*;
 
 public class Question extends JOptionPane {
     //instance variables
@@ -10,12 +8,14 @@ public class Question extends JOptionPane {
     //sets up the icon used in the questions
     static ImageIcon icon;
     static {
+        //make origional image icon from the file
         ImageIcon bigIcon = new ImageIcon("resources/logo.png");
+        //convert it to an image so it can be scaled
         Image bigImage = bigIcon.getImage();
+        //scale it down
         Image smallImage = bigImage.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+        //make a new, smaller, image icon
         icon = new ImageIcon(smallImage);
-
-
     }
 
     public Question(String question, Answer[] answers) {
@@ -31,8 +31,6 @@ public class Question extends JOptionPane {
     //asks the question
     public int[] ask() {
         //returns the array of ints from the chosen answer
-        //makes an icon to use in the question popup
-        ImageIcon bigIcon = new ImageIcon("resources/logo.png");
         return answers[showOptionDialog(null,question,"Question",JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,icon,answers,0)].getResult();
     }
 }
